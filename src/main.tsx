4b9a58c90ed148404c2e5a5bbfc5eaada4268b08
Vanzzy027 <22514@student.embuni.ac.ke>
2025-12-16 18:36:27 +0300

    Refresh

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+//import { PersistGate } from 'redux-persist/lib/integration/react.js';
 import { Toaster } from 'sonner';
 import App from './App';
 import { store, persistor } from './store/store';
 import './index.css';
 
 // ADD THIS BLOCK: Disable logs in production environment
-if (import.meta.env.PROD) {
+if (import.meta.env) {
   console.log = () => {};
   console.debug = () => {};
   console.info = () => {};
