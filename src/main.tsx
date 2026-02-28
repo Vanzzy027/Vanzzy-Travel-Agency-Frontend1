66bf5517a17533e451b07ea88a93294d8423ac37
Evans Kariuki <118306655+Vanzzy027@users.noreply.github.com>
2026-02-28 04:11:10 +0300

    Clean up commented code in main.tsx
    Removed commented-out code for React and Redux setup.

--- src/main.tsx
+++ src/main.tsx
@@ -32,34 +32,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </Provider>
   </React.StrictMode>
 );
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { Toaster } from 'sonner';
-// import App from './App';
-// import { store, persistor } from './store/store';
-// import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistor}>
-//         <App />
-//         <Toaster 
-//           richColors 
-//           position="top-right"
-//           theme="light"
-//           expand={true}
-//           duration={4000}
-//         />
-//       </PersistGate>
-//     </Provider>
-//   </React.StrictMode>
-// );
-
-
-
-
-
